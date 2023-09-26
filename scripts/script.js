8cd3de4410a4acd290958761bc5f9ccd9928a7e2
irem011 <143966809+irem011@users.noreply.github.com>
2023-09-26 21:13:35 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,14 @@
 // JavaScript Document
 console.log("hi");
 
+
+
+var menuButton = document.querySelector("header li:last-of-type button");
+var deNav = document.querySelector("header nav");
+
+
+menuButton.onclick = toggleMenu;
+
+function toggleMenu() {
+	deNav.classList.toggle("open");
+}
